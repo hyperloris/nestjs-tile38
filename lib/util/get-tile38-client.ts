c4372a615231b671407b3fcfd0ae07608b96be3e
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2022-09-14 12:45:56 +0200

    fix: wrong import path

--- lib/util/get-tile38-client.ts
+++ lib/util/get-tile38-client.ts
@@ -1,5 +1,6 @@
 import { Tile38 } from '@tiermobility/tile38-ts';
-import { Tile38ModuleOptions } from 'lib/interfaces';
+
+import { Tile38ModuleOptions } from './../interfaces';
 
 export function getTile38Client(options: Tile38ModuleOptions): Tile38 {
   const { url, followerUrl } = options;
